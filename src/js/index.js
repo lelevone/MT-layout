6d583a1c290843eced8f00efeeef7db6ba1fcf6c
lelevone <boss.roma-novikov@mail.ru>
2021-12-02 17:34:33 +0300

    fix some layout bugs

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-async function asnc() {
-  const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await resp.json();
-  console.log(data);
-}
-class Button{
-  static btn = ""
-}
-asnc();
